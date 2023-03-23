e4d84fc2f54bea98edebaf1c9c6f2a01b51530cf
Leyla Callahan <leyla.callahan@yahoo.com>
2023-03-22 23:13:56 -0400

    2

--- Assets/script.js
+++ Assets/script.js
@@ -1,4 +1,10 @@
     var apiKey = "41efa49b5c3d44802e760cf096eb6503";
+    var nameofCity = document.getElementById("city_name");
+    var todayTemperature = document.getElementById("temptoday");
+    var todayWind = document.getElementById("windtoday");
+    var todayHumidity = document.getElementById("humiditytoday");
+    var singleday = document.getElementsByClassName("singleday");
+    var cityUl = document.getElementById("savedcitieslist");
 //     var inputValue=document.getElementById("city");
 //     var searchButton=document.getElementById("search");
 //     searchButton.addEventListener("click", (event)=>{
@@ -34,6 +40,7 @@ function getCoordinate(cityName) {
                         longitude = longitude.toString();
 
                         getWeather(latitude, longitude, cityName);
+                        saveCities(cityName);
                         
                     }
                 })
@@ -50,16 +57,66 @@ function getWeather(lat, lon, cityName){
         return response.json()
     })
     .then(function(data){
-        console.log(data.list)
-        // for(i = 0; i < 6; i++){
-        //     var temp = data.list[i].temp.day;
-        //     console.log(temp)
-        //     // if (i === 0){
+        // console.log(data.list[0].main)
+        for(i = 0; i < 6; i++){
+            var temp = data.list[i].main.temp;
+            var wind = data.list[i].wind.speed;
+            var humidity = data.list[i].main.humidity;
+            var utcdate = data.list[i].dt;
+            var date = new Date(utcdate * 1000);
+	        var day = date.getDate();
+	        var month = date.getMonth() + 1;
+	        var year = date.getFullYear();
+            var dateFormatted = month.toString() + '/' + day.toString() + '/' + year.toString();
+            // console.log(wind)
+            
+            if (i === 0){
+                nameofCity.innerText = cityName + "  (" + dateFormatted + ")";
+                todayTemperature.innerText = "Temp: " + temp.toString() + "F";
+                todayWind.innerText = "Wind: " + wind.toString() + "MPH";
+                todayHumidity.innerText = "Humidity:" + humidity.toString() + "%";
+            }else{
+                var index = i-1;
+                singleday[index].innerHTML = "<h6>" + dateFormatted + "</h6><p>Temp: " + temp.toString() + "</p><p>Wind: " + wind.toString() + " MPH</p> <p>Humidity: " + humidity.toString() + "%</p>"
+            }
 
-        //     // }
-        // }
+        }
     })
 }
 
+function setWeather() {
+    var cityName = document.getElementById("search").value;
+    getCoordinate(cityName);
+}
 
-getCoordinate("Atlanta")
+function saveCities(searchvalue) {
+    if (localStorage.savedCity == undefined) {
+        var savedCity = [searchvalue];
+        localStorage.savedCity = JSON.stringify(savedCity);
+    };
+
+    var newSearch = [searchvalue];
+    var currentList = localStorage.savedCity;
+    var savedCity = JSON.parse(currentList);
+    if (!savedCity.includes(searchvalue)) {
+        savedCity = savedCity.concat(newSearch);
+        localStorage.savedCity = JSON.stringify(savedCity);
+    }
+    getCities();
+
+}
+
+function getCities() {
+    var currentList = localStorage.savedCity;
+    var savedCity = JSON.parse(currentList);
+    var newCityList = [];
+    for (var i = 0; i < savedCity.length; i ++) {
+        newCityList +='<li onclick=getCoordinate("'+ savedCity[i] +'") value="' + savedCity[i] + '">' + savedCity[i] + '</li>';
+    }
+
+    cityUl.innerHTML = newCityList;
+
+}
+
+getCities();
+
